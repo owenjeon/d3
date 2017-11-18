cd4503b80d474fe1600376b6c56bf0dbb714d84c
owen <owen@zigbang.com>
2017-11-18 15:25:37 +0900

    file 정리

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
-const ProgressPlugin = require('webpack/lib/ProgressPlugin');
-let webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    "index": [
-      "./wp/src/index.ts"
-    ]
+    "index": "./wp/src/ts1.ts",
+    //"another": './wp/src/another-module.ts'
   },
   output: {
-    "path": path.join(process.cwd(), "wp/dist"),
+    "path": path.join(__dirname, "wp/dist"),
     "filename": "[name].bundle.js",
-    "chunkFilename": "[id].chunk.js"
+    "chunkFilename": "[name].chunk.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "wp/dist"),
+    compress: true,
+    port: 9000
   },
   module: {
     rules: [
@@ -22,27 +26,18 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"]
   },
   plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    }),
+    //new HTMLWebpackPlugin({title:'Code Spliting'}), //index.html 파일 생성
+    //new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'}),// 공통으로 쓰이는 의존 모듈들을 별개의 js파일로 분리해서 번들링 함
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        // warnings: false, // 콘솔 창에 출력되는 게 보기 귀찮다면 요 놈을 주석 제거하면 된다.
+        warnings: true, // 콘솔 창에 출력되는 게 보기 귀찮다면 요 놈을 주석 제거하면 된다.
         unused: true // 요 놈이 핵심
       },
       mangle: false,    // DEMO ONLY: Don't change variable names.(난독화)
       beautify: true,   // DEMO ONLY: Preserve whitespace (가독성 좋게 함)
       output: {
-        comments: true  // DEMO ONLY: Helpful comments (주석 삭제 안 함)
+        comments: false  // DEMO ONLY: Helpful comments (주석 삭제 안 함)
       }
     })
-  ],
-  resolve: {
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'wp/src')
-    ],
-    extensions: ['.ts', '.js']
-  }
+  ]
 };
